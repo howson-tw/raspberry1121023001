7e456cfd800c953f24648b94e3cc5405fed4cede
B7 <B7@example.com>
2023-10-25 14:47:41 +0800

    modift_1121025008

---
+++ project1/lesson7.py
@@ -0,0 +1,13 @@
+# 從 gpiozero 模組當中載入 LED 功能
+from gpiozero import LED
+from time import sleep
+
+led = LED(27)
+# led = LED("GPIO27")   # 給 GPIO 腳位，跟上一行意義相同！
+# led = LED("J8:13")    # 定義為樹莓派的腳位
+
+while True:
+  led.on()
+  sleep(1)
+  led.off()
+  sleep(1)
